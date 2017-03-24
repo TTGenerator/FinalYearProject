5cd1674baf54a46ea158484a03a6cbee352fd556
JayaniSumudini <msjs.sumudini@gmail.com>
2017-03-24 23:21:12 +0530

    add loader

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import {LoaderService} from "./common/services/loader/loaderService";
 
 @Component({
   moduleId: module.id,
   selector: 'my-app',
   templateUrl: 'app.component.html'
 })
-export class AppComponent  {};
+export class AppComponent  {
+  showLoader: boolean;
+  constructor(
+    private loaderService: LoaderService) {
+  }
+  ngOnInit() {
+    this.loaderService.status.subscribe((val: boolean) => {
+      this.showLoader = val;
+    });
+  }
+
+}
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,29 +1,38 @@
-import { NgModule }      from '@angular/core';
+import {NgModule, CUSTOM_ELEMENTS_SCHEMA}      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { FormsModule }    from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing }  from './app.routing';
+import { MaterialModule } from '@angular/material';
+import '../../node_modules/hammerjs/hammer.js';
 
 import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {LoginService} from "./common/services/login/loginService";
 import {LoginRouteGuard} from "./common/services/login/loginRouteGuard";
+import { LoaderService } from './common/services/loader/loaderService';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    //ui module
+    MaterialModule.forRoot()
   ],
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent
   ],
-  providers: [LoginService,LoginRouteGuard],
-  bootstrap: [AppComponent]
+  providers: [LoginService,LoginRouteGuard,LoaderService],
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
+
 })
 
 export class AppModule { }
