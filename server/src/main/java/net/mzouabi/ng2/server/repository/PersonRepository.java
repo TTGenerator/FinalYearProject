bb01e3fe6b7a4f3835e06ba0673e968a28f01c1b
JayaniSumudini <msjs.sumudini@gmail.com>
2017-05-30 11:16:49 +0530

    create classroom service csll

--- server/src/main/java/net/mzouabi/ng2/server/repository/PersonRepository.java
+++
@@ -1,6 +0,0 @@
-package net.mzouabi.ng2.server.repository;
-
-import net.mzouabi.ng2.server.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long>, PersonRepositoryCustom {}
--- server/src/main/java/net/mzouabi/ng2/server/repository/PersonRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package net.mzouabi.ng2.server.repository;
-
-public interface PersonRepositoryCustom {
-
-	public int countCustom();
-
-}
