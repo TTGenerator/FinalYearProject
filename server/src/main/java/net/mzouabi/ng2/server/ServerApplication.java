0c24ebab2ae11c27ba49ec97b30712120da7dd07
JayaniSumudini <msjs.sumudini@gmail.com>
2017-06-12 16:09:41 +0530

    deploy to seperate tomcat

--- server/src/main/java/net/mzouabi/ng2/server/ServerApplication.java
+++ server/src/main/java/net/mzouabi/ng2/server/ServerApplication.java
@@ -4,11 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 @CrossOrigin(origins = "http://localhost:4200")
-public class ServerApplication {
+public class ServerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
+	@Bean
+	public TomcatEmbeddedServletContainerFactory tomcatEmbeddedServletContainerFactory() {
+		return new TomcatEmbeddedServletContainerFactory();
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ServerApplication.class);
+	}
+
 }
